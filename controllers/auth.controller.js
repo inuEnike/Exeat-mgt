beb1c296f6643f54b3da07ac4f6f06addd2dccd9
Inu Enike George <105659884+inuEnike@users.noreply.github.com>
2024-06-24 15:48:26 +0100

    Update auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -22,7 +22,10 @@ export const Signup = async (req, res, next) => {
     } else if (password !== repeatPassword) {
       return res.json({ message: "Password should match!!" });
     }
-
+  const user = Auth.findOne({email})
+    if(user){
+      return res.json({ message: "An Account has been registered with the email" });
+    }
     const salt = await bcrypt.genSalt(10);
 
     const hashedPassword = await bcrypt.hash(password, salt);
